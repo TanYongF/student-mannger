d5d28836b9e28ee4a992615c72e8bb3f16e7807e
B19040229谭永锋 <tan13621251388@gmail.com>
2021-11-15 08:44:52 +0800

    init

---
+++ src/main/java/xyz/tanyongfeng/studentmannagersystem/StudentMannagerSystemApplication.java
@@ -0,0 +1,13 @@
+package xyz.tanyongfeng.studentmannagersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMannagerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentMannagerSystemApplication.class, args);
+    }
+
+}
